b97122be2bcae3c612a29e25fd7bf7abef742ced
T360-v6-JAVM-s07 <DanNagy@molitdgbs.com>
2021-12-13 13:33:24 +0100

    Registration_2

--- src/main/java/claaatructureio/Registration.java
+++ src/main/java/claaatructureio/Registration.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Registration {
     public static void main(String[] args) {
-        System.out.println("What is your name?");
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
         String name = scanner.nextLine();
 
         System.out.println("What is your E-mail address?");
-        Scanner emailaddress = new Scanner(System.in);
-        String email = emailaddress.nextLine();
+        String email = scanner.nextLine();
 
         System.out.println("Your name: " + name);
         System.out.println("Your E-mail address: " + email);
